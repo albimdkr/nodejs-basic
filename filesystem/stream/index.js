ced7ec5359bce5128025841a8e357c0817c0c0ac
albimdkr <albimudakar04@gmail.com>
2023-09-10 10:26:21 +0700

    adding R/C/W file

---
+++ filesystem/stream/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+// Writable Stream
+const writableStream = fs.createWriteStream('output.txt');
+writableStream.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+
+// Readable Stream
+const readableStream = fs.createReadStream('output.txt', {
+    highWaterMark: 15
+});
+
+readableStream.on('readable', () => {
+    try {
+        process.stdout.write(`[${readableStream.read()}]`);
+    } catch (error){
+        //catch error here
+    }
+});
+
+readableStream.on('end', () => {
+    console.log('Done');
+});
+
